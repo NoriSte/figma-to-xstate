bcde8c61e2c0edd6294ee923c1ea8f09647fd5b5
Stefano Magni <nori.ste.magni@gmail.com>
2023-10-17 17:26:36 +0200

    Test the drag navigation among frames

--- src/generators.test.ts
+++ src/generators.test.ts
@@ -107,4 +107,63 @@ describe('createXStateV4StateMachineOptions', () => {
     `.trim()
     )
   })
+
+  it('When passed with the options generated from the "Click and drag frame navigation" Figma file, then use the writer to compose a the corresponding state machine', () => {
+    const writer = generateNewWriter()
+    const generatorOptions: GeneratorOptions = {
+      writer,
+      currentPageName: 'Page 1',
+      simplifiedFrames: [
+        { id: '1:2', name: 'Frame 1' },
+        { id: '1:3', name: 'Frame 2' },
+        { id: '1:4', name: 'Frame 3' },
+        { id: '1:5', name: 'Frame 4' },
+      ],
+      interactiveNodes: [
+        {
+          node: { id: '1:8' },
+          parentFrameId: '1:2',
+          triggerType: 'ON_CLICK',
+          destinationFrameId: '1:3',
+          generatedName: 'Click to navigate to Frame 2',
+        },
+        {
+          node: { id: '1:12' },
+          parentFrameId: '1:3',
+          triggerType: 'ON_DRAG',
+          destinationFrameId: '1:2',
+          generatedName: 'Drag to navigate to Frame 1',
+        },
+      ],
+    }
+
+    createXStateV4StateMachineOptions(generatorOptions)
+
+    expect(writer.toString()).toEqual(
+      `
+{
+  id: 'Page_1',
+  initial: 'Frame_1',
+  states: {
+    Frame_1:{
+      on: {
+        ON_CLICK_CLICK_TO_NAVIGATE_TO_FRAME_2: 'Frame_2',
+      }
+    },
+    Frame_2:{
+      on: {
+        ON_DRAG_DRAG_TO_NAVIGATE_TO_FRAME_1: 'Frame_1',
+      }
+    },
+    Frame_3:{
+      type: 'final'
+    },
+    Frame_4:{
+      type: 'final'
+    },
+  }
+}
+    `.trim()
+    )
+  })
 })
