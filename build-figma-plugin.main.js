5b7345c0b9bf22848037d690cf094cde4d185941
Mohammad Bagher Abiat <zorofight94@gmail.com>
2023-11-04 12:47:33 +0330

    fix build

--- build-figma-plugin.main.js
+++ build-figma-plugin.main.js
@@ -3,7 +3,7 @@
  * @param {import('esbuild').BuildOptions} buildOptions
  * @returns  {import('esbuild').BuildOptions} esbuild options
  */
-module.exports = function (buildOptions) {
+export default function (buildOptions) {
   return {
     ...buildOptions,
     mainFields: ['module', 'main', 'browser'],
